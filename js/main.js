001ff77587cb38b1ddbd2a106ceb1f93df16b7b7
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-09-05 22:58:05 +0200

    push
    push

--- js/main.js
+++ js/main.js
@@ -44,18 +44,6 @@ up.onclick = function () {
     behavior: 'smooth',
   });
 };
-// $(window).scroll(function() {
-//   if ($(this).scrollTop() >= 100) {
-//     $('#return-to-top').fadeIn(200);
-//   } else {
-//     $('#return-to-top').fadeOut(200);
-//   }
-// });
-// $('#return-to-top').on('click', function() {
-//   $('body,html').animate(
-//     {
-//       scrollTop: 0,
-//     },
-//     500
-//   );
-// });
+// --------------------------------------
+
+
