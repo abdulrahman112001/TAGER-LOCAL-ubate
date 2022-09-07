f1e5639847f65fbf2482150569b2b5ddf3a7d75d
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-09-07 02:42:28 +0200

    push
    push

--- js/main.js
+++ js/main.js
@@ -44,3 +44,19 @@ up.onclick = function () {
     behavior: 'smooth',
   });
 };
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+},
+});
