da476909d574dae56b2d9e3ddfec664dc52a5bc8
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-09-11 21:46:18 +0200

    push
    push

--- js/main.js
+++ js/main.js
@@ -60,3 +60,56 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
 },
 });
+// sow more
+$(function() {
+  var increment = 12;
+  var startFilter = 0;
+  var endFilter = increment;
+  var $this = $('.items');
+  var elementLength = $this.find('div').length;
+  $('.listLength').text(elementLength);
+
+  if (elementLength > 2) {
+      $('.buttonToogle').show();
+  }
+  $('.items .item').slice(startFilter, endFilter).addClass('shown');
+  $('.shownLength').text(endFilter);
+  $('.items .item').not('.shown').hide();
+  $('.buttonToogle .showMore').on('click', function() {
+      if (elementLength > endFilter) {
+          startFilter += increment;
+          endFilter += increment;
+          $('.items .item').slice(startFilter, endFilter).not('.shown').addClass('shown').toggle(500);
+          $('.shownLength').text((endFilter > elementLength) ? elementLength : endFilter);
+          if (elementLength <= endFilter) {
+              $(this).remove();
+          }
+      }
+
+  });
+});
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+try {
+  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+    return true;
+  }).catch(function(e) {
+    var carbonScript = document.createElement("script");
+    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+    carbonScript.id = "_carbonads_js";
+    document.getElementById("carbon-block").appendChild(carbonScript);
+  });
+} catch (error) {
+  console.log(error);
+}
