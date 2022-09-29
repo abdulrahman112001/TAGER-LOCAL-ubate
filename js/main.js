85865e1308e3661983ad3f01ba6f1fa3ea52d00e
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-09-30 00:56:00 +0200

    push
    push

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@ $('.main-carwsel').owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
   responsive: {
     0: {
       items: 1,
@@ -32,6 +36,10 @@ $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
   responsive: {
     0: {
       items: 3,
